b16561c99b3417cc345a9ab9b447d3c7830e17d5
Kkalla <dkswns333@gmail.com>
2018-01-15 20:39:38 +0900

    Add sample.py - Modify model.sample()

--- model.py
+++ model.py
@@ -93,30 +93,30 @@ class RNN_Model():
 #        optimizer = tf.train.AdamOptimizer(self.lr)
 #        self.train_op = optimizer.apply_gradients(zip(grads,tvars))
 #       #############################################################
-        def sample(self, sess, chars, vocab, num=200,prime='0001',sampling_type=1):
-            state = sess.run(self.cell.zero_state(1,tf.float32))
-            
-            for char in prime[:-1]:
-                x = np.zeros((1,1))
-                x[0,0] = vocab[char]
-                feed = {self.input_data:x,self.initial_state:state}
-                [state] = sess.run([self.final_state],feed)
-            
-            def weighted_pick(weights):
-                t = np.cumsum(weights)
-                s = np.sum(weights)
-                return int(np.searchsorted(t,np.random.rand(1)*s))
-            
-            ret = prime
-            char = prime[-1]
-            for n in range(num):
-                x = np.zeros((1,1))
-                x[0,0] = vocab[char]
-                feed = {self.input_data:x, self.initial_state: state}
-                [probs,state] = sess.run([self.probs, self.final_state],feed_dict = feed)
-                p = probs[0]
-                sample = weighted_pick(p)
-                pred = chars[sample]
-                ret += pred
-                char = pred
-            return ret
+    def sample(self, sess, chars, vocab, num=200,prime='0001',sampling_type=1):
+        state = sess.run(self.cell.zero_state(1,tf.float32))
+        
+        for char in prime[:-1]:
+            x = np.zeros((1,1))
+            x[0,0] = vocab[char]
+            feed = {self.input_data:x,self.initial_state:state}
+            [state] = sess.run([self.final_state],feed)
+        
+        def weighted_pick(weights):
+            t = np.cumsum(weights)
+            s = np.sum(weights)
+            return int(np.searchsorted(t,np.random.rand(1)*s))
+        
+        ret = prime
+        char = prime[-1]
+        for n in range(num):
+            x = np.zeros((1,1))
+            x[0,0] = vocab[char]
+            feed = {self.input_data:x, self.initial_state: state}
+            [probs,state] = sess.run([self.probs, self.final_state],feed_dict = feed)
+            p = probs[0]
+            sample = weighted_pick(p)
+            pred = chars[sample]
+            ret.append(pred)
+            char = pred
+        return ret
---
+++ sample.py
@@ -0,0 +1,45 @@
+"""
+Generating samples
+
+@author: kkalla
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+
+import os
+import argparse
+
+from six.moves import cPickle
+import tensorflow as tf
+
+from model import RNN_Model
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--save_dir',type=str,default='save')
+parser.add_argument('-n','--num_samples',type=int,default=10)
+parser.add_argument('--prime',type=str,default=['0001','0002'])
+parser.add_argument('--sample',type=int,default=1)
+
+def sample(args):
+    with open(os.path.join(args.save_dir,'config.pkl'),'rb') as f:
+        saved_args = cPickle.load(f)
+    with open(os.path.join(args.save_dir,'chars_vocab.pkl'),'rb') as f:
+        chars, vocab = cPickle.load(f)
+
+    model = RNN_Model(saved_args,mode='INFER')
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        saver = tf.train.Saver(tf.global_variables())
+        ckpt = tf.train.get_checkpoint_state(args.save_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess = sess, save_path = ckpt.model_checkpoint_path)
+            print(model.sample(sess,chars,vocab,args.num_samples,args.prime,args.sample))
+
+def main():
+    args = parser.parse_args()
+    sample(args)
+
+if __name__ =='__main__':
+    main()
