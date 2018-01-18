177d7bd47c98020a48e957f7b806e22cc7081e18
kkalla <dkswns333@gmail.com>
2018-01-18 08:43:27 +0000

    aa

--- split_test.py
+++ split_test.py
@@ -13,11 +13,11 @@ shop_data = pd.read_csv('data/shopping_sorted.csv',dtype={'ID':str,'PD_S_C':str,
                                              'RCT_NO':str,'DE_DT':str})
 shop_data['DE_DT'] = pd.to_datetime(shop_data.DE_DT,format='%Y-%m-%d')
 test = pd.DataFrame()
-for i in shop_data.ID.unique():
-    test.append(shop_data[shop_data.ID == i][-5:])
+#for i in shop_data.ID.unique():
+#    test.append(shop_data[shop_data.ID == i][-5:])
 
-test.to_csv('data/test_seq.csv',index=False)
-print('test_seq done!!')
+#test.to_csv('data/test_seq.csv',index=False)
+#print('test_seq done!!')
 train = pd.DataFrame()
 for i in shop_data.ID.unique():
     train.append(shop_data[shop_data.ID == i][:-5])
