3d0eafd0652e11c5276b430bb8ff789329e3d6fc
Kkalla <dkswns333@gmail.com>
2018-01-19 20:22:17 +0900

    Modify split_test.py

--- split_test.py
+++ split_test.py
@@ -12,40 +12,38 @@ import time
 
 import pandas as pd
 
-<<<<<<< HEAD
 shop_data = pd.read_csv('data/02_shopping_tran.txt',dtype={'ID':str,'PD_S_C':str,'RCT_NO':str,'DE_DT':str})
-non_shop_data = pd.read_csv('data/03_non_shopping_tran.txt')
 shop_data['DE_DT'] = pd.to_datetime(shop_data.DE_DT,format='%Y-%m-%d')
+
+#non_shop_data = pd.read_csv('data/03_non_shopping_tran.txt')
 #non_shop_data['CRYM'] = pd.to_datetime(non_shop_data.CRYM,format='%Y%m')
+
 #sort by date and hr
 shop_data = shop_data.sort_values(by=['ID','DE_DT','DE_HR'])
 #non_shop_data = non_shop_data.sort_values(by=['ID','CRYM'])
-shop_data.head()
 
-test = pd.DataFrame()
-#for i in shop_data.ID.unique():
-#    test.append(shop_data[shop_data.ID == i][-5:])
-
-#test.to_csv('data/test_seq.csv',index=False)
-#print('test_seq done!!')
-# Store last 5 items to testset
-#test = pd.DataFrame()
-#for i in shop_data.ID.unique():
-#    test.append(shop_data[shop_data.ID == i][-5:])
-#
-#test.to_csv('data/test_seq.csv',index=False)
-#print('test_seq done!!')
-
-#train = pd.DataFrame()
-#for i in shop_data.ID.unique():
-#    train.append(shop_data[shop_data.ID == i][:-5])
-#
-#train.to_csv('data/train_seq.csv',index=False)
-#print('train_seq done!!')
-
+# split train and test
 grouped = shop_data.groupby(by='ID')
 groups = grouped.groups
 keys = shop_data.ID.unique()
+
+test = pd.DataFrame()
+i = 0
+start = time.time()
+for key in keys:
+    aa = groups.get(key)[-5:]
+    aa = shop_data.iloc[aa,:]
+    test = test.append(aa)
+    if i%1000==0:
+        end = time.time()
+        print('step %d'%(i))
+        print('in time %f'%(end-start))
+        print(key)
+    i += 1
+print('test_seq done!!')
+test.to_csv('data/test_seq.csv',index=False)
+
+
 train = pd.DataFrame()
 i = 0
 start = time.time()
