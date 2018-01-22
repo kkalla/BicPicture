985e6db3cac25e1100ef673c2bb1574d4f3f4557
Kkalla <dkswns333@gmail.com>
2018-01-22 15:04:10 +0900

    Modifying preprocessing

--- misc/preprocessing.py
+++ misc/preprocessing.py
@@ -17,6 +17,31 @@ train.head()
 #subsetting
 train_sub = train[['ID','PD_S_C','DE_DT','DE_HR']].copy()
 train_sub = train_sub.drop_duplicates()
+train_sub.reset_index(inplace=True)
 
+#Split train and target
+# train .7 test .3
+grouped = train_sub.groupby('ID')
+ids = train_sub.ID.unique()
+src_dataset = pd.DataFrame()
+tgt_dataset = pd.DataFrame()
+i = 0
+for id in ids:
+    if i % 1000==0:
+        print('# of steps: %d, id: %s'%(i+1,id))
+    indexes = grouped.groups.get(id).values
+    subset = train_sub.iloc[indexes,:]
+    length_subset = subset.shape[0]
+    src_size = int(length_subset*.7)
+    tgt_size = length_subset - src_size
+    src_dataset = src_dataset.append(subset.iloc[:src_size,:])
+    tgt_dataset = tgt_dataset.append(subset.iloc[-tgt_size:,:])
+    i += 1
 #Save to csv
+print('saving train_processed')
 train_sub.to_csv('../data/train_processed.csv',index=False)
+print('saving src_dataset.csv')
+src_dataset.to_csv('../data/src_dataset.csv',index=False)
+print('saving trg_dataset')
+tgt_dataset.to_csv('../data/tgt_dataset.csv',index=False)
+
